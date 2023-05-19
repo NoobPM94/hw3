798a9b63f25ff215e8bed2a62422d0aa3fa19a7c
Shubham Prajapati <shubham.prajapati@kellogg.northwestern.edu>
2023-05-19 17:51:57 +0000

    finally did it lol

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    @place = Place.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" => params["place_id"]})
+    @post["place_id"] = @place["id"] 
   end
 
   def create
@@ -16,8 +17,6 @@ class PostsController < ApplicationController
     @post["posted_on"] = params["post"]["posted_on"]
     @post["description"] = params["post"]["description"]
     @post["place_id"] = params["post"]["place_id"]
-
-    @post["place_id"] = @place["id"]
 
     @post.save
 
